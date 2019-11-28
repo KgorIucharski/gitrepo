7c779b33a67223c80f57a88743cc8b44b5a77a9d
KgorIucharski <igorkucharski@lo1.sandomierz.pl>
2019-11-28 12:34:04 +0100

    c++

--- c++/równanie.cpp
+++ c++/równanie.cpp
@@ -5,9 +5,26 @@
 
 using namespace std;
 
-int main(int argc, char **argv)
-{
-	
+int main(int argc, char **argv) {
+    int a;
+    int b;
+    float x;
+	cout << "Witaj w C++";
+   
+    cout << "podaj liczbę a: ";
+    cin >> a;	
+    cout << "podaj liczbę b: ";
+    cin >> b;
+    if (a == 0) {
+        if (b == 0) {
+            cout << "nieskończenie wiele rozwiązań";
+            } else {
+                cout << "równaie sprzeczne"; 
+            }
+    } else {
+        x = -b / (float)a;
+        cout << x;
+    }
 	return 0;
 }
 
