25f56f136ffd448b35b05df8a0457cb0305d9482
KgorIucharski <igorkucharski@lo1.sandomierz.pl>
2019-12-05 12:35:57 +0100

    liczonko

---
+++ c++/placa.cpp
@@ -0,0 +1,22 @@
+/*
+ */
+
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	float n, s, p;
+    cout << "podaj ilość wypracowanych nadgodzin: " << endl;
+    cin >> n;
+    cout << "podaj stawkę: " << endl;
+    cin >> s;
+    
+    if (l <= 30); {
+        n*s;
+        cout << p
+        }
+    
+	return 0;
+}
+
---
+++ c++/wieksza.cpp
@@ -0,0 +1,29 @@
+/*
+
+ */
+
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int a, b;
+    cout << "podaj liczbę a: " << endl;
+    cin >> a;
+    cout << "podaj liczbę b: " << endl;
+    cin >> b;
+    
+    if (a > b) {
+        cout << a
+        }
+        
+     else if (a < b) {
+        cout << b
+        }
+        
+    else { 
+        cout << "są równe" << endl;
+    }
+	return 0;
+}
+
