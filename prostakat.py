19994f56e7d708b26009237b857f2ee84fc55f2d
KgorIucharski <igorkucharski@lo1.sandomierz.pl>
2019-12-18 11:40:48 +0100

    ta

---
+++ prostakat.py
@@ -0,0 +1,13 @@
+
+
+
+def main(args):
+    return 0
+    wys = int(input("podaj wysokość prostokąta: ")) 
+    szer = int(input("podaj szerokość prostokąta: "))
+    znak = int(input("podaj znak: "))
+    print(znak * wys)
+    print(znak * szer)
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
