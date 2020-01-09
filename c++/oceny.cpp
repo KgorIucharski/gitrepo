8e35d131a5659d3e1a5e4f9d214e38156d6fad42
KgorIucharski <igorkucharski@lo1.sandomierz.pl>
2020-01-09 12:37:27 +0100

    yhm

--- c++/oceny.cpp
+++ c++/oceny.cpp
@@ -6,18 +6,41 @@
 #include <iostream>
 using namespace std;
 
-
-int main(int argc, char **argv)
-{
-	int a, b, c;
-    cout << "podaj 3 oceny: " << endl;
-    cin >> a >> b >> c;
-    if (a == 0) {
-        if (b == 0) {
-            if (c == 0) {
-                cout << "błędna liczba"; }
-            }
+void pobierzOceny (int tb[], int n) {
+    int ocena = 0;
+    int i = 0;
+    while (n > 0)    
+    //for (int i=0; i < n; i++) 
+    {
+        cout << "podaj ocenę: " ;
+        cin >> ocena;
+        if (ocena > 0 && ocena < 7) {
+            tb[i] = ocena;
+            i++;
+            n--;
+        }   
+    }
+    
+float liczSrednia(int tb[], int n) {
+    int suma = 0
+        for (int i = 0; i < n; i++) {
+        cout << tb[i] << " ";
+        
         }
+}
+    
+    
+}
+        
+        }
+    }
+int main(int argc, char **argv)
+{   int n;
+    cout << "ile ocen podasz? ";
+    cin >> n;
+    int oceny[n];
+    pobierzOceny(oceny, n);
+    
 	return 0;
 }
 
