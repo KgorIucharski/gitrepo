84568851fc24ee6c4f2d6d7d8175645bf1e9b527
KgorIucharski <igorkucharski@lo1.sandomierz.pl>
2020-01-15 11:40:03 +0100

    oceny

--- c++/oceny.cpp
+++ c++/oceny.cpp
@@ -10,7 +10,6 @@ void pobierzOceny (int tb[], int n) {
     int ocena = 0;
     int i = 0;
     while (n > 0)    
-    //for (int i=0; i < n; i++) 
     {
         cout << "podaj ocenę: " ;
         cin >> ocena;
@@ -20,27 +19,27 @@ void pobierzOceny (int tb[], int n) {
             n--;
         }   
     }
-    
+}
+
 float liczSrednia(int tb[], int n) {
     int suma = 0
-        for (int i = 0; i < n; i++) {
-        cout << tb[i] << " ";
-        
+    for (int i = 0; i < n; i++) {
+        //cout << tb[i] << " ";
+        suma = suma + tb[i];
         }
-}
-    
-    
-}
-        
-        }
+        return (float)suma / (float)n;
     }
+    
+        
+    
 int main(int argc, char **argv)
 {   int n;
     cout << "ile ocen podasz? ";
     cin >> n;
     int oceny[n];
     pobierzOceny(oceny, n);
-    
+    //float srednia = liczSrednia(oceny, n);
+    cout << "Srednia ocen: " << liczSrednia(oceny, n) << endl;
 	return 0;
 }
 
