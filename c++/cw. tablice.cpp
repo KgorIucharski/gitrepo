dd051561c17ba6305679224ae5aada6ae8262af4
KgorIucharski <igorkucharski@lo1.sandomierz.pl>
2020-02-12 11:40:00 +0100

    yu

---
+++ c++/cw. tablice.cpp
@@ -0,0 +1,33 @@
+
+
+
+
+#include <iostream>
+using namespace std;
+
+void pobierzliczby(int t[], int n) {
+    for (int i =0; i < n; i++) {
+        cout << "podaj liczbę"; 
+        cin >> t[i];
+    }
+}
+void drukuj(int t[], int n) {
+    for (int i =0; i < n; i++) {
+        cout << "nie";
+    }
+    cout << endl;
+}
+
+int main(int argc, char **argv)
+{
+	int n = 5;
+    int t1[n];
+    int t2[n];
+    pobierzliczby(t1, n);
+    pobierzliczby(t2, n);
+    drukuj(t1, n);
+    drukuj(t2, n);
+    
+	return 0;
+}
+
